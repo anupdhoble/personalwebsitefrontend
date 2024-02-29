eab28195fb4f8ec26c913b52c31ba12cfb5f7689
Anup Dhoble <83175840+anupdhoble@users.noreply.github.com>
2024-02-29 18:43:49 +0530

    Refactor Blogs component to improve readability and remove unnecessary code

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -24,7 +24,7 @@ const Blogs = ({ isLogin }) => {
                 const formattedBlogs = data.map(blog => ({
                     ...blog,
                     createdAt: new Date(blog.createdAt).toLocaleDateString()
-                })).reverse(); // Reverse the order of blogs
+                })).reverse();
                 setBlogs(formattedBlogs);
             } else {
                 throw new Error("Failed to fetch blogs");
@@ -34,8 +34,39 @@ const Blogs = ({ isLogin }) => {
         }
     };
 
-    // Rest of the code...
-
+    return (
+        <>
+            <div className="title-content max-width-1">
+                <div className="title-content-left">
+                    <h1 className="blog-title-welcome">Welcome to Blogs!!</h1>
+                    <p>Create Your Own Blogs By Logging In!!!</p>
+                    <p className="thoughts">I think of us as journalists; the medium we work in is blogging. <br />--Joshua Marshall</p>
+                </div>
+                <div className="title-content-right">
+                    <img src={blogPageImg} alt="home.png" />
+                </div>
+            </div>
+            <BlogController isLogin={isLogin} />
+            <div className="blog-articles footer-fix max-width-1">
+                <h2 className="color2">Featured Articles</h2>
+                {blogs.map((blog) => (
+                    <article key={blog._id} className="blog-article"> {/* Use _id for key */}
+                        <div className="article-header">
+                            <span className="article-title">{blog.title}</span>
+                            <span className="article-right">
+                                <div className="article-author">@{blog.author}</div>
+                                <div className="article-time">{blog.createdAt}</div>
+                            </span>
+                        </div>
+                        <div className="hr-nav m-auto"><hr /></div>
+                        <div className="article-content">
+                            <p>{blog.content}</p>
+                        </div>
+                    </article>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default Blogs;
