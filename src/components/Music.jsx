c82d8b2adbd66a90a900bf1b83ee5ac0f24a0045
Anup Dhoble <83175840+anupdhoble@users.noreply.github.com>
2024-07-19 11:13:52 +0530

    reminder added

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,11 +1,11 @@
 import '../styles/music.css';
-
+//Remember!!!! i have used elfsight for music and added a script at index.html in public , remeber to remove it when in future i wish to remove the widget 
 export default function Music() {
     
 
     return (
         <div className='musicApp'>
-           
+           //Remember!!!! i have used elfsight for music and added a script at index.html in public , remeber to remove it when in future i wish to remove the widget 
            <div className="elfsight-app-25670a45-fcbc-4429-a0e8-1e47a41ec179" data-elfsight-app-lazy></div>
         </div>
     )
